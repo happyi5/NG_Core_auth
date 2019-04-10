1beddd204baeae8946867d244c1207ed734206d9
Susanna Hajek <susanna.hajek@gmail.com>
2019-04-10 06:04:45 -0500

    Add project files.

---
+++ NG_Core_Auth/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NG_Core_Auth.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) 
+        {
+            _userManager = userManager;
+            _signManager = signInManager;
+        }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formdata)
+        {
+            // will hold all the errors related to registration.
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formdata.Email,
+                UserName = formdata.Username,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+                //sending confirmation email
+                return Ok(new { username = user.UserName, email= user.Email, status = 1, message="Registration is Successful"});
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
+    
+
+        }
+
+    }
+}
---
+++ NG_Core_Auth/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NG_Core_Auth.Models;
+
+namespace NG_Core_Auth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+
+        //create roles for or application
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(
+                new {Id = "1", Name ="Admin", NormalizedName = "ADMIN" },
+                new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" },
+                new { Id = "3", Name = "Moderator", NormalizedName = "MODERATOR" }
+                );
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
